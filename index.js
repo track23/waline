957887ce4b6839feb4e6b248258dd496e09ad79e
track13 <58624410+track23@users.noreply.github.com>
2022-08-27 22:52:39 +0800

    delete ua,ip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Application = require('@waline/vercel');
 
 module.exports = Application({
   async postSave(comment) {
-    // do what ever you want after save comment
+    delete comment.ua;
+    delete comment.ip;
   },
 });
